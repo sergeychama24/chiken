ac31a3af3fd5c8ee6301b881ef056d9d9558fd49
sergeychama24 <sergeychama24@gmail.com>
2024-02-12 21:20:37 +0300

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: { main: './src/index.js' },
@@ -22,6 +23,16 @@ module.exports = {
                 test: /\.js$/,
                 use: 'babel-loader',
                 exclude: '/node_modules/'
+            },
+            {
+                test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+                type: 'asset/resource'
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, {
+                    loader: 'css-loader'
+                }]
             }
         ]
     },
@@ -30,6 +41,7 @@ module.exports = {
             template: './src/index.html'
         }),
         new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin()
     ]
 
 }
